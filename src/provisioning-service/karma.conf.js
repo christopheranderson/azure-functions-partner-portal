a230069b6d335d6c28eae1154f0ecf0b193dca6e
jwallra <jawal@microsoft.com>
2016-06-14 16:12:37 -0700

    Adding Karma and related dependencies

---
+++ src/provisioning-service/karma.conf.js
@@ -0,0 +1,26 @@
+//jshint strict: false
+module.exports = function(config) {
+  config.set({
+
+    basePath: './app',
+
+    files: [
+      '**/*.module.js',
+      '*!(.module|.spec).js',
+      '**/*.spec.js'
+    ],
+
+    autoWatch: true,
+
+    frameworks: ['jasmine'],
+
+    browsers: ['Chrome', 'Firefox'],
+
+    plugins: [
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      'karma-jasmine'
+    ]
+
+  });
+};

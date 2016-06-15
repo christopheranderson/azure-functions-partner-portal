10adcb3fca43f9d3210417da055fc10b010b8c0e
Christopher Anderson <christopheranderson@Christophers-MacBook-Pro.local>
2016-06-15 13:15:10 -0700

    Add context class

---
+++ src/provisioning-service/bin/lib/common-functions.ts
@@ -0,0 +1,25 @@
+class Context {
+    public bindings: any;
+    public bindingData: any;
+    public invocationId: any;
+
+    private isDone: boolean = false;
+
+    log(data) {
+        console.log(data);
+    }
+
+    done(err, results?) {
+        if(this.isDone) {
+            return;
+        } else if(err) {
+            console.log('Function failed');
+            console.log(err);
+        } else {
+            console.log('Function completed');
+            console.log(results);
+        }
+    }
+}
+
+export { Context }

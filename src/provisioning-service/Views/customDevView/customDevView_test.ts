fee1c6bd26f51c002ff1d995832cd5d47040325a
jwallra <jawal@microsoft.com>
2016-06-16 14:58:34 -0700

    ts-ifying views

---
+++ src/provisioning-service/Views/customDevView/customDevView_test.ts
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('provisionerPortal.customDevView module', function() {
+
+  beforeEach(module('provisionerPortal.customDevView'));
+
+  describe('customDevView controller', function(){
+
+    it('should ....', inject(function($controller) {
+      //spec body
+      var customDevViewCtrl = $controller('customDevViewCtrl');
+      expect(customDevViewCtrl).toBeDefined();
+    }));
+
+  });
+});
